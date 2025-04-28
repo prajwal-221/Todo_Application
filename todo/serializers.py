652772182dfafffbdc3cd77326f90be16553627f
prajwal-221 <prajwaln@heaptrace.com>
2025-04-28 18:59:58 +0530

    Update serializers.py

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,7 +1,15 @@
+from rest_framework import serializers
 from rest_framework_mongoengine.serializers import DocumentSerializer
-from .models import Task, Subtask
+from .models import Task, Subtask, Comment, ActivityLog
+
 
 class TaskSerializer(DocumentSerializer):
+    linked_task = serializers.PrimaryKeyRelatedField(
+        queryset=Task.objects,
+        allow_null=True,
+        required=False
+    )
+
     class Meta:
         model = Task
         fields = '__all__'
@@ -10,4 +18,16 @@ class TaskSerializer(DocumentSerializer):
 class SubtaskSerializer(DocumentSerializer):
     class Meta:
         model = Subtask
+        fields = '__all__'
+
+
+class CommentSerializer(DocumentSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class ActivityLogSerializer(DocumentSerializer):
+    class Meta:
+        model = ActivityLog
         fields = '__all__'
