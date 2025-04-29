d0e4dabeaf315a5cb216042e114ee4cd54c6cb96
prajwal-221 <prajwaln@heaptrace.com>
2025-04-29 13:36:27 +0530

    Update serializers.py

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,24 +1,37 @@
-from rest_framework import serializers
 from rest_framework_mongoengine.serializers import DocumentSerializer
+from rest_framework import serializers
 from .models import Task, Subtask, Comment, ActivityLog
 
 
 class TaskSerializer(DocumentSerializer):
-    linked_task = serializers.PrimaryKeyRelatedField(
-        queryset=Task.objects,
-        allow_null=True,
-        required=False
-    )
-
     class Meta:
         model = Task
         fields = '__all__'
 
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['id'] = str(instance.id)
+        if instance.linked_task:
+            rep['linked_task'] = str(instance.linked_task.id)
+        return rep
+
 
 class SubtaskSerializer(DocumentSerializer):
+    parent_task = serializers.PrimaryKeyRelatedField(
+        queryset=Task.objects.all(),
+        required=False,
+    )
+
     class Meta:
         model = Subtask
         fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['id'] = str(instance.id)
+        if instance.parent_task:
+            rep['parent_task'] = str(instance.parent_task.id)
+        return rep
 
 
 class CommentSerializer(DocumentSerializer):
@@ -26,8 +39,22 @@ class CommentSerializer(DocumentSerializer):
         model = Comment
         fields = '__all__'
 
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['id'] = str(instance.id)
+        if instance.task:
+            rep['task'] = str(instance.task.id)
+        return rep
+
 
 class ActivityLogSerializer(DocumentSerializer):
     class Meta:
         model = ActivityLog
         fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['id'] = str(instance.id)
+        if instance.task:
+            rep['task'] = str(instance.task.id)
+        return rep
