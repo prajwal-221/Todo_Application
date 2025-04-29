914f1e77c847e375d34313e6210d8593e31239bf
prajwal-221 <prajwaln@heaptrace.com>
2025-04-29 13:36:49 +0530

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,17 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (
-    TaskViewSet,
-    SubtaskViewSet,
-    CommentViewSet,
-    ActivityLogViewSet,
-)
+from .views import TaskViewSet, SubtaskViewSet, CommentViewSet, ActivityLogViewSet
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
 router.register(r'subtasks', SubtaskViewSet, basename='subtask')
 router.register(r'comments', CommentViewSet, basename='comment')
-router.register(r'activity-log', ActivityLogViewSet, basename='activitylog')
+router.register(r'logs', ActivityLogViewSet, basename='activitylog')
 
 urlpatterns = [
     path('', include(router.urls)),
