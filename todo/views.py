eb938a220417d962e5f4446ebd0d65911aad08e9
prajwal-221 <prajwaln@heaptrace.com>
2025-04-29 13:37:08 +0530

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework_mongoengine import viewsets
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework import status
+from mongoengine import ValidationError as MEValidationError
 
 from .models import Task, Subtask, Comment, ActivityLog
 from .serializers import (
@@ -10,55 +12,76 @@ from .serializers import (
     ActivityLogSerializer,
 )
 
-
 class TaskViewSet(viewsets.ModelViewSet):
+
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
+    def perform_create(self, serializer):
+        try:
+            serializer.save()
+        except MEValidationError as e:
+            raise ValidationError({"detail": str(e)})
+
     def perform_update(self, serializer):
-        old = Task.objects.get(id=serializer.instance.id)
-        new_status = serializer.validated_data.get('status', old.status)
-        serializer.save()
-        # log status change
-        if new_status != old.status:
-            ActivityLog.objects.create(
-                action="status_update",
-                task=serializer.instance,
-                details=f"Status changed from {old.status} to {new_status}"
-            )
+        try:
+            serializer.save()
+        except MEValidationError as e:
+            raise ValidationError({"detail": str(e)})
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        title = instance.title
+        self.perform_destroy(instance)
+        return Response(
+            {"detail": f"Task '{title}' has been deleted."},
+            status=status.HTTP_200_OK
+        )
 
 
 class SubtaskViewSet(viewsets.ModelViewSet):
-    queryset = Subtask.objects.all()
     serializer_class = SubtaskSerializer
 
+    def get_queryset(self):
+        task_pk = self.kwargs.get('task_pk')
+        if task_pk:
+            return Subtask.objects.filter(parent_task=task_pk)
+        return Subtask.objects.all()
+
+    def perform_create(self, serializer):
+        try:
+            task_pk = self.kwargs.get('task_pk')
+            if task_pk:
+                parent = Task.objects.get(pk=task_pk)
+                serializer.save(parent_task=parent)
+            else:
+                serializer.save()
+        except Task.DoesNotExist:
+            raise ValidationError({"parent_task": "Specified task does not exist."})
+        except MEValidationError as e:
+            raise ValidationError({"detail": str(e)})
+
     def perform_update(self, serializer):
-        old = Subtask.objects.get(id=serializer.instance.id)
-        new_completed = serializer.validated_data.get('completed', old.completed)
-        serializer.save()
-        if new_completed != old.completed:
-            ActivityLog.objects.create(
-                action="status_update",
-                subtask=serializer.instance,
-                details=f"Completion changed from {old.completed} to {new_completed}"
-            )
+        try:
+            serializer.save()
+        except MEValidationError as e:
+            raise ValidationError({"detail": str(e)})
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        title = instance.title
+        self.perform_destroy(instance)
+        return Response(
+            {"detail": f"Subtask '{title}' has been deleted."},
+            status=status.HTTP_200_OK
+        )
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
-    def perform_create(self, serializer):
-        comment = serializer.save()
-        ActivityLog.objects.create(
-            action="comment_added",
-            task=comment.task,
-            subtask=comment.subtask,
-            details=f"Comment added: {comment.content}"
-        )
 
-
-class ActivityLogViewSet(viewsets.ReadOnlyModelViewSet):
-    """Read-only listing of all log entries."""
-    queryset = ActivityLog.objects.order_by('-timestamp')
+class ActivityLogViewSet(viewsets.ModelViewSet):
+    queryset = ActivityLog.objects.all()
     serializer_class = ActivityLogSerializer
