f08451800d6fce61f1aac5cd12e9264d23b95e3e
prajwal-221 <prajwaln@heaptrace.com>
2025-04-29 13:38:09 +0530

    Update urls.py

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -1,24 +1,28 @@
-"""
-URL configuration for todo_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedSimpleRouter
+
+from todo.views import TaskViewSet, SubtaskViewSet, CommentViewSet, ActivityLogViewSet
+
+# 1) Base router for top‐level endpoints
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet, basename='task')
+router.register(r'subtasks', SubtaskViewSet, basename='subtask')
+router.register(r'comments', CommentViewSet, basename='comment')
+router.register(r'logs', ActivityLogViewSet, basename='activitylog')
+
+# 2) Nested router: /tasks/{task_pk}/subtasks/
+tasks_router = NestedSimpleRouter(router, r'tasks', lookup='task')
+tasks_router.register(
+    r'subtasks',
+    SubtaskViewSet,
+    basename='task-subtasks'
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('todo.urls')),
+    # All app endpoints under /api/
+    path('api/', include(router.urls)),
+    path('api/', include(tasks_router.urls)),
 ]
-
